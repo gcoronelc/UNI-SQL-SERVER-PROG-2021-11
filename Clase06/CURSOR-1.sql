0899bb5d66a8f3fe3e020a24ae9b6bf78172e45a
Gustavo Coronel <gcoronelc@gmail.com>
2021-12-10 20:15:39 -0500

    Cursores

--- Clase06/CURSOR-1.sql
+++ Clase06/CURSOR-1.sql
@@ -3,11 +3,14 @@ DECLARE @cur_id int, @cur_nombre varchar(100), @cur_precio money;
 
 DECLARE cur_cursos CURSOR 
 FOR SELECT cur_id, cur_nombre, cur_precio FROM dbo.CURSO; 
+SELECT CURSOR_STATUS('global','cur_cursos') AS 'Después de declarar';
 
 OPEN cur_cursos;
- 
+SELECT CURSOR_STATUS('global','cur_cursos') AS 'Después de Abrir';
+
 FETCH NEXT FROM cur_cursos INTO @cur_id, @cur_nombre, @cur_precio; 
 PRINT CAST(@cur_id AS VARCHAR(5)) + ' - ' + @cur_nombre + ' - ' + CAST(@cur_precio AS VARCHAR(12)); 
+SELECT CURSOR_STATUS('global','cur_cursos') AS 'Después de FETCH';
 
 FETCH NEXT FROM cur_cursos INTO @cur_id, @cur_nombre, @cur_precio; 
 PRINT CAST(@cur_id AS VARCHAR(5)) + ' - ' + @cur_nombre + ' - ' + CAST(@cur_precio AS VARCHAR(12)); 
--- Clase06/CURSOR-2.sql
+++ Clase06/CURSOR-2.sql
@@ -1,22 +1,24 @@
 
 DECLARE @cur_id int, @cur_nombre varchar(100), @cur_precio money;
 
-DECLARE cur_cursos CURSOR 
+DECLARE cur_cursos CURSOR FORWARD_ONLY KEYSET
 FOR SELECT cur_id, cur_nombre, cur_precio FROM dbo.CURSO; 
 
 OPEN cur_cursos;
  
 FETCH NEXT FROM cur_cursos INTO @cur_id, @cur_nombre, @cur_precio; 
-PRINT CAST(@cur_id AS VARCHAR(5)) + ' - ' + @cur_nombre + ' - ' + CAST(@cur_precio AS VARCHAR(12)); 
-
 WHILE(@@FETCH_STATUS=0)
 BEGIN
+	PRINT CAST(@@CURSOR_ROWS AS VARCHAR(5)) + ' - ' + CAST(@cur_id AS VARCHAR(5)) + ' - ' + @cur_nombre + ' - ' + CAST(@cur_precio AS VARCHAR(12)); 
 	FETCH NEXT FROM cur_cursos INTO @cur_id, @cur_nombre, @cur_precio; 
-	PRINT CAST(@cur_id AS VARCHAR(5)) + ' - ' + @cur_nombre + ' - ' + CAST(@cur_precio AS VARCHAR(12)); 
 END;
 
 CLOSE cur_cursos; 
 
 DEALLOCATE cur_cursos;
 GO
+
+
+
+
 
